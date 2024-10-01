d79a462a65a34dcca501432097ad2547a81194d4
allan <allansaleh95@gmail.com>
2024-10-01 14:48:23 -0400

    initial commit

---
+++ Python Syntax, Conditionals, Lists/workshop.py
@@ -0,0 +1,24 @@
+# Python Syntax
+# camelCase, snake_case, PascalCase, UPPER_CASE
+
+# Python Conditionals (if, elif, else)
+# falsy values
+# empty sequences and collections (lists, tuples, strings, dictionaries, sets), zero in every numeric type, None, and False
+
+# comparison operators
+# <, <=, >, >=, ==, !=
+
+# logical AND & OR
+# nested if & input function
+
+# Python Lists: Stores multiple values in a single variable
+
+# indexing
+
+# slicing (start(includes): stop(excludes): step)
+
+# add to list
+
+# remove from list using .pop(0), .remove('student 1'), del, .clear()
+
+# membership checks with "in" & "not in"
